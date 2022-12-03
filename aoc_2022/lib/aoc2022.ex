e3ee699e86c0fdc61d07e50c7f1fff3bc13d6e5c
Erki Treier <erki.treier@glia.com>
2022-12-03 14:12:38 +0200

    restructure inputs

--- aoc_2022/lib/aoc2022.ex
+++ aoc_2022/lib/aoc2022.ex
@@ -2,6 +2,7 @@ defmodule Aoc2022 do
   def all do
     day1()
     day2()
+    day3()
   end
 
   def day1 do
@@ -17,6 +18,15 @@ defmodule Aoc2022 do
     IO.puts("______")
     IO.puts("Part 1: #{Day2.get_final_score}")
     IO.puts("Part 2: #{Day2.get_final_score_2}")
+    IO.puts("------\n")
+  end
+
+
+  def day3 do
+    IO.puts("Day 3")
+    IO.puts("______")
+    IO.puts("Part 1: #{Day3.answer1}")
+    IO.puts("Part 2: #{Day3.answer2}")
     IO.puts("------\n")
   end
 end
