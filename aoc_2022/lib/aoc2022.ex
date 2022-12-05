da969a8772d5d31aa6daedc0c215b80db8c05c54
Erki Treier <erki.treier@glia.com>
2022-12-05 23:53:40 +0200

    Add time only output

--- aoc_2022/lib/aoc2022.ex
+++ aoc_2022/lib/aoc2022.ex
@@ -1,48 +1,64 @@
 defmodule Aoc2022 do
   def all do
-    day1()
-    day2()
-    day3()
-    day4()
-    day5()
+    day1(true)
+    day2(true)
+    day3(true)
+    day4(true)
+    day5(true)
   end
 
-  def print_result(day, {time1, answer1},  {time2, answer2}) do
+  def times do
+    day1(false)
+    day2(false)
+    day3(false)
+    day4(false)
+    day5(false)
+  end
+
+  def print_result(day, part1,  part2, include_answers) do
     IO.puts("Day #{day}")
     IO.puts("______")
-    IO.puts("Part 1: #{answer1} (#{time1}us)")
-    IO.puts("Part 2: #{answer2} (#{time2}us)")
+    print_part(1, part1, include_answers)
+    print_part(2, part2, include_answers)
     IO.puts("------\n")
   end
 
-  def day1 do
+  def print_part(num, {time, answer}, include_answers) do
+    if include_answers do
+      IO.puts("Part #{num}: #{answer} TIME: #{time} microseconds")
+    else
+      IO.puts("Part #{num}: TIME: #{time} microseconds")
+    end
+  end
+
+  def day1(include_answers) do
     part1 = :timer.tc(fn -> Day1.most_calories end)
     part2 = :timer.tc(fn -> Day1.sum_of_top_3 end)
-    print_result(1, part1, part2)
+    print_result(1, part1, part2, include_answers)
   end
 
-  def day2 do
+  def day2(include_answers) do
     part1= :timer.tc(fn -> Day2.get_final_score end)
     part2 = :timer.tc(fn -> Day2.get_final_score_2 end)
-    print_result(2, part1, part2)
+    print_result(2, part1, part2, include_answers)
   end
 
 
-  def day3 do
+  def day3(include_answers) do
     part1= :timer.tc(fn -> Day3.answer1 end)
     part2 = :timer.tc(fn -> Day3.answer2 end)
-    print_result(3, part1, part2)
+    print_result(3, part1, part2, include_answers)
   end
 
-  def day4 do
+  def day4(include_answers) do
     part1= :timer.tc(fn -> Day4.answer1 end)
     part2 = :timer.tc(fn -> Day4.answer2 end)
-    print_result(4, part1, part2)
+    print_result(4, part1, part2, include_answers)
   end
 
-  def day5 do
+  def day5(include_answers) do
     part1= :timer.tc(fn -> Day5.answer1 end)
     part2 = :timer.tc(fn -> Day5.answer2 end)
-    print_result(5, part1, part2)
+    print_result(5, part1, part2, include_answers)
   end
 end
