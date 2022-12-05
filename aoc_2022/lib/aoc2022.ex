2ec9c894da2fe075ddd856decc2f247cde57c341
Erki Treier <erki.treier@glia.com>
2022-12-05 23:15:17 +0200

    Add timing and restructure printout

--- aoc_2022/lib/aoc2022.ex
+++ aoc_2022/lib/aoc2022.ex
@@ -4,46 +4,45 @@ defmodule Aoc2022 do
     day2()
     day3()
     day4()
+    day5()
+  end
+
+  def print_result(day, {time1, answer1},  {time2, answer2}) do
+    IO.puts("Day #{day}")
+    IO.puts("______")
+    IO.puts("Part 1: #{answer1} (#{time1}us)")
+    IO.puts("Part 2: #{answer2} (#{time2}us)")
+    IO.puts("------\n")
   end
 
   def day1 do
-    IO.puts("Day 1")
-    IO.puts("______")
-    IO.puts("Part 1: #{Day1.most_calories}")
-    IO.puts("Part 2: #{Day1.sum_of_top_3}")
-    IO.puts("------\n")
+    part1 = :timer.tc(fn -> Day1.most_calories end)
+    part2 = :timer.tc(fn -> Day1.sum_of_top_3 end)
+    print_result(1, part1, part2)
   end
 
   def day2 do
-    IO.puts("Day 2")
-    IO.puts("______")
-    IO.puts("Part 1: #{Day2.get_final_score}")
-    IO.puts("Part 2: #{Day2.get_final_score_2}")
-    IO.puts("------\n")
+    part1= :timer.tc(fn -> Day2.get_final_score end)
+    part2 = :timer.tc(fn -> Day2.get_final_score_2 end)
+    print_result(2, part1, part2)
   end
 
 
   def day3 do
-    IO.puts("Day 3")
-    IO.puts("______")
-    IO.puts("Part 1: #{Day3.answer1}")
-    IO.puts("Part 2: #{Day3.answer2}")
-    IO.puts("------\n")
+    part1= :timer.tc(fn -> Day3.answer1 end)
+    part2 = :timer.tc(fn -> Day3.answer2 end)
+    print_result(3, part1, part2)
   end
 
   def day4 do
-    IO.puts("Day 4")
-    IO.puts("______")
-    IO.puts("Part 1: #{Day4.answer1}")
-    IO.puts("Part 2: #{Day4.answer2}")
-    IO.puts("------\n")
+    part1= :timer.tc(fn -> Day4.answer1 end)
+    part2 = :timer.tc(fn -> Day4.answer2 end)
+    print_result(4, part1, part2)
   end
 
   def day5 do
-    IO.puts("Day 5")
-    IO.puts("______")
-    IO.puts("Part 1: #{Day5.answer1}")
-    IO.puts("Part 2: #{Day5.answer2}")
-    IO.puts("------\n")
+    part1= :timer.tc(fn -> Day5.answer1 end)
+    part2 = :timer.tc(fn -> Day5.answer2 end)
+    print_result(5, part1, part2)
   end
 end
